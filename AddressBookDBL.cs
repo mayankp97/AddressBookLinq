ead609ba2b626cf9f9b56b89303e81884ff3e68f
Mayank Purohit <mayankpurohit2012@gmail.com>
2020-11-12 09:37:32 +0530

    [Mayank] Add method to insert values to table

--- AddressBookDBL.cs
+++ AddressBookDBL.cs
@@ -21,5 +21,17 @@ namespace AddressBookLinq
             dataTable.Columns.Add("EmailID", typeof(string));
         }
 
+        public void InsertValues()
+        {
+            dataTable.Rows.Add("Mayank", "Purohit", "Near Heaven", "cochi", "kerela", "155545", "9865656565", "m@gmail.com");
+            dataTable.Rows.Add("Mahesh", "Purohit", "Near Hell", "chennai", "TN", "1658788", "95554545", "mp@gmail.com");
+            dataTable.Rows.Add("Manish", "Purohit", "Near chinatown", "bengaluru", "Karnataka", "165301", "4545454545", "ma@gmail.com");
+            dataTable.Rows.Add("Mukesh", "Purohit", "Near jawahar circle", "jodhpur", "Rajasthan", "6654554", "9845454545", "mu@gmail.com");
+            dataTable.Rows.Add("Meena", "Purohit", "Near kala bazar", "kolkata", "WB", "5454545", "7784845455", "m@gmail.com");
+            dataTable.Rows.Add("Minali", "Purohit", "Near lal bazar", "puri", "Orissa", "545454", "8876988998", "mi@gmail.com");
+
+
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ namespace AddressBookLinq
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to address book system Linq");
+
+            var addressBookDB = new AddressBookDBL();
+            addressBookDB.CreateAddressBook();
+            addressBookDB.InsertValues();
         }
     }
 }
